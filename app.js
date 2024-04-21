244811782e847cad8711b1c9d0028e30aa3fbff3
Hugo Hezro <hugohezro@hotmail.com>
2024-04-21 01:10:06 -0300

    mudar tela

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 40;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
+function exibirTextoNaTela(tag, texto,tst) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
